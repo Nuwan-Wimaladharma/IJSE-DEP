679cfa80889839187a6356eb1c585ede5fdcaed3
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-02-02 01:52:17 +0530

    Updated 2023 Feb 01

---
+++ Phase-1/oop/oop-demos/src/lk/ijse/dep10/oop/exercise/Exercise04.java
@@ -0,0 +1,25 @@
+package lk.ijse.dep10.oop.exercise;
+
+public class Exercise04 {
+    public static void main(String[] args) {
+        //Teacher t0; //Actually thus is appeared in stack as Reference t0 ===> Reference t0 = new Teacher() <--- This is ok
+        Teacher t1 = new Teacher("963601999V","Nuwan","0712951059");
+        t1.printDetails();
+    }
+}
+class Teacher{
+    String nic;
+    String name;
+    String contact;
+
+    public Teacher (String nic, String name, String contact){
+        super();
+        this.nic = nic;
+        this.name = name;
+        this.contact = contact;
+    }
+    void printDetails(){
+        String name = "Nuwan";
+        System.out.printf("nic=%s, name=%s, contact=%s %n",nic, name, contact);
+    }
+}
