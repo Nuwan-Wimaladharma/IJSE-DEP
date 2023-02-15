df76a6d881aec2d587e177e9e5b4c7af56865d91
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-02-15 23:47:52 +0530

    Set stage of the combo-box-exercise to show

--- Phase-1/java-fx/java-fx-fundamentals/java-fx-combo-box-exercise/src/main/java/lk/ijse/dep10/app/AppInitializer.java
+++ Phase-1/java-fx/java-fx-fundamentals/java-fx-combo-box-exercise/src/main/java/lk/ijse/dep10/app/AppInitializer.java
@@ -1,7 +1,13 @@
 package lk.ijse.dep10.app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class AppInitializer extends Application {
 
@@ -10,7 +16,15 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        URL fxmlFile =getClass().getResource("/view/MainView.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlFile);
+        AnchorPane root = fxmlLoader.load();
 
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Module Selection Form");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 }
