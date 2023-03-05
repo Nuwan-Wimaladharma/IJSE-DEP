c9fd3ccc771b805c3598c56b2e7582c0cc2f001d
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-03-05 13:05:48 +0530

    paint-app completed

--- Phase-1/java-fx/java-fx-fundamentals/java-fx-paint/src/main/java/lk/ijse/dep10/app/controller/MainViewController.java
+++ Phase-1/java-fx/java-fx-fundamentals/java-fx-paint/src/main/java/lk/ijse/dep10/app/controller/MainViewController.java
@@ -2,17 +2,25 @@ package lk.ijse.dep10.app.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ColorPicker;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class MainViewController {
 
+    public TextField txtTextField;
+    public ToolBar tbToolBar;
+    @FXML
+    private TextArea txtTextArea;
+    @FXML
+    private Button btnClearAll;
     @FXML
     private Button btnCircle;
 
@@ -60,76 +68,135 @@ public class MainViewController {
     private boolean text = false;
     private boolean strokeColor = false;
     private boolean fillColor = false;
+    private boolean clearAll = false;
     private double mouseStartX;
     private double mouseStartY;
+    private double startingXOfTextBox;
+    private double startingYOfTextBox;
     private double width;
     private double height;
+    private String typedText = "";
     private WritableImage snapshot;
+
+    public void initialize(){
+        GraphicsContext gc = cnvMain.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0,0,cnvMain.getWidth(), cnvMain.getHeight());
+
+        txtTextArea.textProperty().addListener((value, previous, current) -> {
+            typedText = "";
+            typedText += current;
+        });
+    }
 
 
     @FXML
     void btnDragToolBarOnAction(ActionEvent event) {
-
     }
 
     @FXML
     void btnRectangleOnAction(ActionEvent event) {
+        cnvMain.getScene().setCursor(Cursor.DEFAULT);
+        txtTextArea.setVisible(false);
+        txtTextArea.setDisable(true);
         rectangle = true;
         roundedRectangle = false;
         ellipse = false;
         pencil = false;
+        eraser = false;
+        text = false;
         GraphicsContext gc = cnvMain.getGraphicsContext2D();
         gc.setFill(Color.TRANSPARENT);
     }
 
     @FXML
     void btnRoundedRectangleOnAction(ActionEvent event) {
+        cnvMain.getScene().setCursor(Cursor.DEFAULT);
+        txtTextArea.setVisible(false);
+        txtTextArea.setDisable(true);
         roundedRectangle = true;
         rectangle = false;
         ellipse = false;
         pencil = false;
+        eraser = false;
+        text = false;
         GraphicsContext gc = cnvMain.getGraphicsContext2D();
         gc.setFill(Color.TRANSPARENT);
     }
 
     @FXML
     void btnCircleOnAction(ActionEvent event) {
+        cnvMain.getScene().setCursor(Cursor.DEFAULT);
+        txtTextArea.setVisible(false);
+        txtTextArea.setDisable(true);
         circle = true;
         rectangle = false;
         roundedRectangle = false;
         ellipse = false;
         pencil = false;
+        eraser = false;
+        text = false;
         GraphicsContext gc = cnvMain.getGraphicsContext2D();
         gc.setFill(Color.TRANSPARENT);
     }
 
     @FXML
     void btnEllipseOnAction(ActionEvent event) {
+        cnvMain.getScene().setCursor(Cursor.DEFAULT);
+        txtTextArea.setVisible(false);
+        txtTextArea.setDisable(true);
         ellipse = true;
         rectangle = false;
         roundedRectangle = false;
         circle = false;
         pencil = false;
+        eraser = false;
+        text = false;
         GraphicsContext gc = cnvMain.getGraphicsContext2D();
         gc.setFill(Color.TRANSPARENT);
     }
 
     @FXML
     void btnPencilOnAction(ActionEvent event) {
+        cnvMain.getScene().setCursor(Cursor.DEFAULT);
+        txtTextArea.setVisible(false);
+        txtTextArea.setDisable(true);
         pencil = true;
         rectangle = false;
         roundedRectangle = false;
         circle = false;
         ellipse = false;
+        eraser = false;
+        text = false;
     }
 
     @FXML
     void btnEraserOnAction(ActionEvent event) {
-
+        txtTextArea.setVisible(false);
+        txtTextArea.setDisable(true);
+        eraser = true;
+        rectangle = false;
+        roundedRectangle = false;
+        circle = false;
+        ellipse = false;
+        pencil = false;
+        text = false;
+        Image image = new Image("/img/eraserOnAction.png");
+        cnvMain.getScene().setCursor(new ImageCursor(image));
     }
 
     @FXML
     void btnTextOnAction(ActionEvent event) {
+        GraphicsContext gc = cnvMain.getGraphicsContext2D();
+        cnvMain.getScene().setCursor(Cursor.DEFAULT);
+        text = true;
+        rectangle = false;
+        roundedRectangle = false;
+        ellipse = false;
+        circle = false;
+        pencil = false;
+        eraser = false;
+        clearAll = false;
 
     }
 
@@ -146,13 +213,39 @@ public class MainViewController {
         GraphicsContext gc = cnvMain.getGraphicsContext2D();
         gc.setFill(clrFill.getValue());
     }
-
     @FXML
-    void cnvMainOnMouseClicked(MouseEvent event) {
+    void btnClearAllOnAction(ActionEvent actionEvent) {
+        GraphicsContext gc = cnvMain.getGraphicsContext2D();
+        cnvMain.getScene().setCursor(Cursor.DEFAULT);
+        clearAll = true;
+        rectangle = false;
+        roundedRectangle = false;
+        ellipse = false;
+        circle = false;
+        pencil = false;
+        eraser = false;
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0,0,cnvMain.getWidth(),cnvMain.getHeight());
     }
 
     @FXML
-    void cnvMainOnMouseDragged(MouseEvent event) {
+    public void cnvMainOnMouseClicked(MouseEvent event) {
+        if (text){
+            GraphicsContext gc = cnvMain.getGraphicsContext2D();
+            startingXOfTextBox = event.getX();
+            startingYOfTextBox = event.getY();
+            txtTextArea.setVisible(true);
+            txtTextArea.setDisable(false);
+            gc.strokeText(typedText,txtTextArea.getLayoutX()+5,txtTextArea.getLayoutY()+15);
+            txtTextArea.setLayoutX(startingXOfTextBox);
+            txtTextArea.setLayoutY(startingYOfTextBox);
+            txtTextArea.clear();
+            typedText = "";
+        }
+    }
+
+    @FXML
+    public void cnvMainOnMouseDragged(MouseEvent event) {
         if (rectangle){
             width = event.getX() - mouseStartX;
             height = event.getY() - mouseStartY;
@@ -218,12 +311,47 @@ public class MainViewController {
             }
         }
         if (circle){
+            width = event.getX() - mouseStartX;
+            height = event.getY() - mouseStartY;
 
+            if (width > 0 && height > 0){
+                GraphicsContext gc = cnvMain.getGraphicsContext2D();
+                gc.clearRect(0,0,cnvMain.getWidth(), cnvMain.getHeight());
+                gc.drawImage(snapshot,0,0);
+                gc.strokeOval(mouseStartX,mouseStartY,width,width);
+                gc.fillOval(mouseStartX,mouseStartY,width,width);
+            }
+            if (width < 0 && height > 0){
+                GraphicsContext gc = cnvMain.getGraphicsContext2D();
+                gc.clearRect(0,0,cnvMain.getWidth(), cnvMain.getHeight());
+                gc.drawImage(snapshot,0,0);
+                gc.strokeOval((mouseStartX + width),mouseStartY,(-1 * width),(-1 * width));
+                gc.fillOval((mouseStartX + width),mouseStartY,(-1 * width),(-1 * width));
+            }
+            if (height < 0 && width > 0){
+                GraphicsContext gc = cnvMain.getGraphicsContext2D();
+                gc.clearRect(0,0,cnvMain.getWidth(), cnvMain.getHeight());
+                gc.drawImage(snapshot,0,0);
+                gc.strokeOval(mouseStartX,(mouseStartY+height),width,width);
+                gc.fillOval(mouseStartX,(mouseStartY+height),width,width);
+            }
+            if (height < 0 && width < 0){
+                GraphicsContext gc = cnvMain.getGraphicsContext2D();
+                gc.clearRect(0,0,cnvMain.getWidth(), cnvMain.getHeight());
+                gc.drawImage(snapshot,0,0);
+                gc.strokeOval((mouseStartX + width),(mouseStartY + height),(-1 * width),(-1 * width));
+                gc.fillOval((mouseStartX + width),(mouseStartY + height),(-1 * width),(-1 * width));
+            }
         }
         if (pencil){
             GraphicsContext gc = cnvMain.getGraphicsContext2D();
             gc.lineTo(event.getX(), event.getY());
             gc.stroke();
+        }
+        if (eraser){
+            GraphicsContext gc = cnvMain.getGraphicsContext2D();
+            gc.setFill(Color.WHITE);
+            gc.fillRect(event.getX(), event.getY(), 15,15);
         }
         if (ellipse){
             width = event.getX() - mouseStartX;
@@ -275,5 +403,12 @@ public class MainViewController {
     void cnvMainOnMouseReleased(MouseEvent event) {
     }
 
+    public void btnDragOnMousePressed(MouseEvent mouseEvent) {
+    }
+
+    public void btnDragOnMouseDragged(MouseEvent mouseEvent) {
+        tbToolBar.setLayoutX(mouseEvent.getSceneX());
+        tbToolBar.setLayoutY(mouseEvent.getSceneY());
+    }
 }
 
